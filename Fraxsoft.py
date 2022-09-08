6079556a8282338a12a80b155a2b8ab20433ccd8
GerPS0 <gerarps21@gmail.com>
2022-09-07 20:50:46 -0500

    Ya se logra transición entre pantallas, pero se actualiza si se vuelve a presionar el mismo boton

--- Fraxsoft.py
+++ Fraxsoft.py
@@ -1,5 +1,4 @@
 from msilib.schema import ComboBox
-from queue import Empty
 from tkinter import *
 from tkinter.ttk import Combobox
 from PIL import Image, ImageTk
@@ -20,7 +19,7 @@ class MainWindow:
         super().__init__()
         self.master = master   
         self.bg1()
-        Buttons.mainBtn(self)
+        self.mainBtn()
     
     def bg1(self):
         self.master.geometry("920x720")
@@ -31,9 +30,23 @@ class MainWindow:
         self.background_img = ImageTk.PhotoImage(file = f"background.png")
         background = self.MainW.create_image(475.0, 204.0, image=self.background_img)
     
+    def mainBtn(self):       
+        # image for buttons
+        self.New = ImageTk.PhotoImage(file = f"New.png")
+        self.Load = ImageTk.PhotoImage(file = f"Load.png")
+        self.ImgHelp = ImageTk.PhotoImage(file = f"Help.png")
+        # create button
+        self.NewDesign = Button( image = self.New, borderwidth=0, highlightthickness=0, command=self.btnNew, relief = "flat")
+        self.LoadDesign = Button( image = self.Load, borderwidth=0, highlightthickness=0, command=self.btnLoad, relief = "flat")
+        self.Help = Button( image = self.ImgHelp, borderwidth=0, highlightthickness=0, command=self.btnHelp, relief = "flat")
+        self.NewDesign.place(x = 335, y = 405, width= 280, height=70)
+        self.LoadDesign.place(x = 335, y = 505, width= 280, height=70)
+        self.Help.place(x = 335, y = 605, width= 280, height=70)
+
     def btnNew(self):
         self.MainW.destroy()
-        MainBackgraound(self.master)
+        window = ("1")
+        MainBackgraound(self.master,window)
 
     def btnLoad(self):
         archivo = filedialog.askopenfilename()
@@ -45,18 +58,51 @@ class MainWindow:
         pass     
 
 class MainBackgraound:
-    def __init__(self,master):
+    def __init__(self,master,window):
         super().__init__()
         self.master = master
-        self.bg2()
-        ConverterWindow(self.master,self.MainBG)
+        self.Ventanas(window)
+        #ConverterWindow(self.master,self.MainBG)
+    
     def bg2(self):
         self.master.geometry("1280x720")
         self.master.configure(bg = "#253132")
         self.MainBG = Canvas(self.master,bg = "#253132", height = 720, width = 1280, bd = 0, highlightthickness = 0, relief = "ridge")
         self.MainBG.place(x = 0, y = 0)
-        self.bg2_img = ImageTk.PhotoImage(file = f"background2.png")
-        background = self.MainBG.create_image(640.0, 366.0, image=self.bg2_img)
+    
+    def Ventanas(self,value):
+        self.bg2()
+        self.BgButtons()
+        if value == "1":
+            ConverterWindow(self.master,self.MainBG)
+        if value == "2":
+            DesignWindow(self.master,self.MainBG)
+        
+    def btn_click(self,value):
+        self.value = value
+        print(self.value)
+        self.Ventanas(self.value)
+
+    
+    def BgButtons(self):         
+        # image button
+        self.ConvDesImg = ImageTk.PhotoImage(file = f"ConvDes.png")
+        self.ElemSpecImg = ImageTk.PhotoImage(file = f"ElemSpec.png")
+        self.CtrlDesImg = ImageTk.PhotoImage(file = f"CtrlDes.png")
+        self.CtrlImpImg = ImageTk.PhotoImage(file = f"CtrlImp.png")
+        self.SummaryImg = ImageTk.PhotoImage(file = f"Summary.png")
+        # create button
+        self.ConvDes = Button( image = self.ConvDesImg, borderwidth = 0, highlightthickness = 0, command = lambda:self.btn_click("1"), relief = "flat")
+        self.ElemSpec = Button( image = self.ElemSpecImg, borderwidth = 0, highlightthickness = 0, command = lambda:self.btn_click("2"), relief = "flat")
+        self.CtrlDes = Button( image = self.CtrlDesImg, borderwidth = 0, highlightthickness = 0, command = lambda:self.btn_click("3"), relief = "flat")
+        self.CtrlImp = Button( image = self.CtrlImpImg, borderwidth = 0, highlightthickness = 0, command = lambda:self.btn_click("4"), relief = "flat")
+        self.Summary = Button( image = self.SummaryImg, borderwidth = 0, highlightthickness = 0, command = lambda:self.btn_click("5"), relief = "flat")
+        #set button place
+        self.ConvDes.place(x = 6, y = 90, width = 70, height = 61)
+        self.ElemSpec.place(x = 6, y = 171, width = 70, height = 61)
+        self.CtrlDes.place(x = 6, y = 252, width = 70, height = 61)
+        self.CtrlImp.place(x = 6, y = 333, width = 70, height = 61)
+        self.Summary.place(x = 6, y = 414, width = 70, height = 61)
      
     
 class ConverterWindow:
@@ -64,15 +110,28 @@ class ConverterWindow:
         super().__init__()
         self.master = master
         self.canva = canva
-        Buttons.ConverterBtn(self)
+        self.bg2_img = ImageTk.PhotoImage(file = f"background2.png")
+        background = self.canva.create_image(640.0, 366.0, image=self.bg2_img)
+        self.ConverterBtn()
         self.entryBox1()
-    def btn_clicked(self):
-        pass
+    
+    def ConverterBtn(self):       
+        #image Button
+        self.ApplyImg = ImageTk.PhotoImage(file = f"Apply.png")
+        self.DiscardImg = ImageTk.PhotoImage(file = f"Discard.png")
+        # create button
+        self.Apply = Button( image = self.ApplyImg, borderwidth = 0, highlightthickness = 0, command = self.btn_Apply, relief = "flat")
+        self.Discard = Button( image = self.DiscardImg, borderwidth = 0, highlightthickness = 0, command = self.btn_Discard, relief = "flat")
+        #set button place
+        self.Apply.place(x = 96, y = 360, width = 109, height = 26)
+        self.Discard.place(x = 229, y = 360, width = 109, height = 26)
+
     def btn_Apply(self):
         global Values
         Values = [self.Vin.get(), self.Vo.get(), self.Po.get(), self.Vripple.get(), self.Cripple.get(), self.Topology.get()]
         if Values.__contains__(""):
             messagebox.showwarning(title = "Warning", message="Fill all entry values")
+    
     def btn_Discard(self):
         self.Vin.delete(0,END) 
         self.Vo.delete(0,END)
@@ -80,8 +139,7 @@ class ConverterWindow:
         self.Vripple.delete(0,END)
         self.Cripple.delete(0,END)
         Values = [self.Vin.get(), self.Vo.get(), self.Po.get(), self.Vripple.get(), self.Cripple.get(), self.Topology.get()]
-        
-
+           
     def entryBox1(self):
         # Labels
         self.canva.create_text(165.0, 139.5, text = "Input Voltage    =", fill = "#ffffff", font = ("Calibri", int(12.0)))
@@ -105,60 +163,16 @@ class ConverterWindow:
         self.Po.place(x = 222, y = 188, width = 108, height = 13)
         self.Vripple.place(x = 222, y = 215, width = 108, height = 13)
         self.Cripple.place(x = 222, y = 242, width = 108, height = 13)
-        self.Topology.place(x = 222, y = 269, width = 110, height = 13)
+        self.Topology.place(x = 222, y = 269, width = 108, height = 18)
+    
 
-class Buttons:
-    def __init__(self):
-        pass  
-    def mainBtn(self):       
-        # image for buttons
-        self.New = ImageTk.PhotoImage(file = f"New.png")
-        self.Load = ImageTk.PhotoImage(file = f"Load.png")
-        self.ImgHelp = ImageTk.PhotoImage(file = f"Help.png")
-        # create button
-        self.NewDesign = Button( image = self.New, borderwidth=0, highlightthickness=0, command=self.btnNew, relief = "flat")
-        self.LoadDesign = Button( image = self.Load, borderwidth=0, highlightthickness=0, command=self.btnLoad, relief = "flat")
-        self.Help = Button( image = self.ImgHelp, borderwidth=0, highlightthickness=0, command=self.btnHelp, relief = "flat")
-        self.NewDesign.place(x = 335, y = 405, width= 280, height=70)
-        self.LoadDesign.place(x = 335, y = 505, width= 280, height=70)
-        self.Help.place(x = 335, y = 605, width= 280, height=70)
-    def ConverterBtn(self):       
-       # image for buttons
-        self.BuckImg = ImageTk.PhotoImage(file = f"Buck.png")
-        self.BoostImg = ImageTk.PhotoImage(file = f"Boost.png")
-        self.BBImg = ImageTk.PhotoImage(file = f"BB.png")
-        self.ApplyImg = ImageTk.PhotoImage(file = f"Apply.png")
-        self.DiscardImg = ImageTk.PhotoImage(file = f"Discard.png")
-        
-        self.ConvDesImg = ImageTk.PhotoImage(file = f"ConvDes.png")
-        self.ElemSpecImg = ImageTk.PhotoImage(file = f"ElemSpec.png")
-        self.CtrlDesImg = ImageTk.PhotoImage(file = f"CtrlDes.png")
-        self.CtrlImpImg = ImageTk.PhotoImage(file = f"CtrlImp.png")
-        self.SummaryImg = ImageTk.PhotoImage(file = f"Summary.png")
-        # create button
-        self.Buck = Button( image = self.BuckImg, borderwidth = 0, highlightthickness = 0, command = self.btn_clicked, relief = "flat")
-        self.Boost = Button( image = self.BoostImg, borderwidth = 0, highlightthickness = 0, command = self.btn_clicked, relief = "flat") 
-        self.BB = Button( image = self.BBImg, borderwidth = 0, highlightthickness = 0, command = self.btn_clicked, relief = "flat")
-        self.Apply = Button( image = self.ApplyImg, borderwidth = 0, highlightthickness = 0, command = self.btn_Apply, relief = "flat")
-        self.Discard = Button( image = self.DiscardImg, borderwidth = 0, highlightthickness = 0, command = self.btn_Discard, relief = "flat")
-        
-        self.ConvDes = Button( image = self.ConvDesImg, borderwidth = 0, highlightthickness = 0, command = self.btn_clicked, relief = "flat")
-        self.ElemSpec = Button( image = self.ElemSpecImg, borderwidth = 0, highlightthickness = 0, command = self.btn_clicked, relief = "flat")
-        self.CtrlDes = Button( image = self.CtrlDesImg, borderwidth = 0, highlightthickness = 0, command = self.btn_clicked, relief = "flat")
-        self.CtrlImp = Button( image = self.CtrlImpImg, borderwidth = 0, highlightthickness = 0, command = self.btn_clicked, relief = "flat")
-        self.Summary = Button( image = self.SummaryImg, borderwidth = 0, highlightthickness = 0, command = self.btn_clicked, relief = "flat")
-        #set button place
-        self.Buck.place(x = 109, y = 299, width = 45, height = 20)
-        self.Boost.place(x = 166, y = 299, width = 45, height = 20)
-        self.BB.place(x = 222, y = 299, width = 45, height = 20) 
-        self.Apply.place(x = 96, y = 360, width = 109, height = 26)
-        self.Discard.place(x = 229, y = 360, width = 109, height = 26)
+class DesignWindow:
+    def __init__(self,master,canva):
+        super().__init__()
+        self.master = master
+        self.canva = canva
 
-        self.ConvDes.place(x = 6, y = 90, width = 70, height = 61)
-        self.ElemSpec.place(x = 6, y = 171, width = 70, height = 61)
-        self.CtrlDes.place(x = 6, y = 252, width = 70, height = 61)
-        self.CtrlImp.place(x = 6, y = 333, width = 70, height = 61)
-        self.Summary.place(x = 6, y = 414, width = 70, height = 61)
+    
 
 if __name__ == "__main__":
     root = Tk()
